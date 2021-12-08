7a3633162f410e8f7fc79a89ea50187260a8cd07
koshishrijal <koshishrijal468@gmail.com>
2021-12-08 08:34:59 +0545

    repo initialized

---
+++ src/main/java/com/kosh/csvrestapi/CsvRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.kosh.csvrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kosh/csvrestapi/CsvRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosh.csvrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
